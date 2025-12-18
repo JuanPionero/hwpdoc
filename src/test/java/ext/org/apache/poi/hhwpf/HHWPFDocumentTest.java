4acf196bfc41d556954664ce3399b943620ac3ca
Juan <juanlee0@naver.com>
2025-12-18 17:08:03 +0900

    파일 생성 후 저장에 대한 테스팅

--- src/test/java/ext/org/apache/poi/hhwpf/HHWPFDocumentTest.java
+++ src/test/java/ext/org/apache/poi/hhwpf/HHWPFDocumentTest.java
@@ -4,41 +4,72 @@ import ext.org.apache.poi.hhwpf.model.DocInfo;
 import ext.org.apache.poi.hhwpf.model.storage.BodyText;
 import ext.org.apache.poi.hhwpf.util.factory.HHWPDocumentFactory;
 import ext.org.apache.poi.hpsf.HwpSummaryInformation;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class HHWPFDocumentTest {
-    HHWPFDocument hwpDoc;
-    @BeforeEach
-    void setUp() {
+    static HHWPFDocument hwpDoc;
+    static Path tempFilePath = Paths.get("result", "blank_temp.hwp");
+
+    @BeforeAll
+    static void setUp() {
         try {
             hwpDoc = HHWPDocumentFactory.create();
+            if(Files.exists(tempFilePath)) {
+                Files.delete(tempFilePath);
+            }
         } catch (Exception e) {
+            // 정상적인 테스트 진행 불가 상황.
             throw new RuntimeException(e);
+        }
+    }
+
+    @AfterAll
+    static void afterAll() {
+        hwpDoc = null;
+        if(Files.exists(tempFilePath)) {
+            try {
+                Files.delete(tempFilePath);
+            } catch (IOException e) {
+            }
         }
     }
 
     @Test
     void getDocInfo() {
-        DocInfo docInfo = hwpDoc.getDocInfo();
-        assert null != docInfo : "Blank HWP Doc must have its DocInfo block";
+        final DocInfo docInfo = hwpDoc.getDocInfo();
+        assertNotNull(docInfo, "Blank HWP Doc must have its DocInfo block");
     }
 
     @Test
     void getBodyText() {
-        BodyText bodyText = hwpDoc.getBodyText();
-        assert null != bodyText : "Blank HWP Doc must have its BodyText block";
-        assert null != bodyText.getSectionList() && bodyText.getSectionList().size() > 0 :
-                "Blank HWP Doc must have at least one text section";
-        assert null != bodyText.getSectionList().get(0).getParagraph(0) :
-                "Blank HWP Doc must have at least one text paragraph at the first section";
+        final BodyText bodyText = hwpDoc.getBodyText();
+        assertNotNull( bodyText , "Blank HWP Doc must have its BodyText block");
+        assertTrue( null != bodyText.getSectionList() && bodyText.getSectionList().size() > 0,
+                "Blank HWP Doc must have at least one text section");
+        assertNotNull( bodyText.getSectionList().get(0).getParagraph(0) ,
+                "Blank HWP Doc must have at least one text paragraph at the first section");
     }
 
     @Test
     void getHwpSummaryInformation() {
-        HwpSummaryInformation summaryInformation = hwpDoc.getHwpSummaryInformation();
-        assert null != summaryInformation : "Blank HWP Doc must have its Summary Information";
+        final HwpSummaryInformation summaryInformation = hwpDoc.getHwpSummaryInformation();
+        assertNotNull( summaryInformation, "Blank HWP Doc must have its Summary Information");
+    }
+
+    @Test
+    void writeTo() {
+        try {
+            hwpDoc.writeTo(tempFilePath);
+            assertTrue( Files.exists(tempFilePath), "Could not write HWP file");
+        } catch (Exception e) {
+            assertTrue(false, "Could not write HWP file");
+        }
     }
 }
